f9140dce00a4c73d9236e8a9a7117b3b18e70db0
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-04-08 11:53:45 +0800

    新增 C#

---
+++ CSharp/Assets/Class1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;           // 引用 Unity API(函式庫:Unity 屬性、功能...)
+
+// 類別 class 定義一個物件
+// 語法 : class 類別名稱
+// : MonoBehaviour 可以將此腳本掛在 Unity 物件上
+// 類別必須放在物件上才會執行
+public class Class1 : MonoBehaviour
+{
+    // 定義類別成員
+    // C#
+    // 1.大小寫不一樣
+    // 2.括號成對出現 () [] {} '' "" <>
+
+    // 事件 : 在特定時間會以指定次數執行的方法 (名稱為藍色)
+    // 開始事件 : 播放遊戲時執行一次  -  初始設定
+    private void Start()
+    {
+        // 輸出("文字訊息");
+        // C# 結尾為分號 ;
+        print("Hey");
+    }
+}
