8d076104c15a1e2b8035cc5ba77f1ecbf5726eef
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-05-19 15:09:35 +0800

    第六章：靜態、API與靜態、非靜態成員

---
+++ CSharp/Assets/Script/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // 一般欄位 (非靜態)
+    // 屬於物件：每個物件都是獨立的
+    public float hp;
+
+    // 靜態欄位
+    // 屬於類別：所有物件共用
+    public static float speed = 3;
+
+    // 一般方法 (非靜態)
+    public void Walk()
+    {
+        print(name + " 走路中...");
+    }
+
+    // 靜態方法
+    // 靜態方法內只能存取靜態成員
+    public static void Stop()
+    {
+        // hp = 100;  (錯誤)
+        speed = 0;
+        print("所有怪物的速度： " + speed);
+    }
+}
---
+++ CSharp/Assets/Script/Practice6.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Practice6 : MonoBehaviour
+{
+    [Header("練習 2")]
+    public SpriteRenderer picture;
+    [Header("練習 4")]
+    public Rigidbody sphere;
+
+    private void Start()
+    {
+        // 練習 1 設定遊戲速度 - 靜態屬性
+        Time.timeScale = 2;
+
+        // 練習 2 翻轉圖片 - 非靜態屬性
+        picture.flipX = true;
+
+        // 練習 3 去小數點 - 靜態方法
+        print("去小數點： " + Mathf.Floor(1.23456f));
+
+        // 練習 4 添加推力 - 非靜態方法
+        sphere.AddForce(0, 800, -800);
+    }
+}
